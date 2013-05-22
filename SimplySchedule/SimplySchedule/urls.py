ed805b2397b179c0d6d5afe498b25af78e59ff2d
jac <jacalata@gmail.com>
2013-05-22 00:33:05 -0700

    Create toy Django pages
    up to http://www.djangobook.com chapter 3

--- SimplySchedule/SimplySchedule/urls.py
+++ SimplySchedule/SimplySchedule/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
+from SimplySchedule.views import hello, current, homepage, hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+
 urlpatterns = patterns('',
+    #  /hello = hello world
+    url(r'^hello/$', hello),
+
+    # site root is also hello world for now
+    url(r'^$', homepage),
+
+    # Django tutorials
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+
     # Examples:
     # url(r'^$', 'SimplySchedule.views.home', name='home'),
     # url(r'^SimplySchedule/', include('SimplySchedule.foo.urls')),
