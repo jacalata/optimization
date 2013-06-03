c8c4791f73dc544b5436860cd723c13012e50501
jac <jacalata@gmail.com>
2013-06-03 01:41:09 -0700

    fix static dirs with correct directory

--- SimplySchedule/SimplySchedule/settings.py
+++ SimplySchedule/SimplySchedule/settings.py
@@ -73,9 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.    
-    PROJECT_PATH+"/../SimplySchedule".replace('\\','/'),
-    PROJECT_PATH+"/../books".replace('\\','/'),
-    PROJECT_PATH+"/../upload".replace('\\','/'),
+    PROJECT_PATH+"/..".replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
