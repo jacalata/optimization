07f077ec7f78f69191805973a2a0604a3e49377b
jac <jacalata@gmail.com>
2013-06-03 01:33:31 -0700

    fix static urls for azure?

--- SimplySchedule/SimplySchedule/settings.py
+++ SimplySchedule/SimplySchedule/settings.py
@@ -72,7 +72,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    # Don't forget to use absolute paths, not relative paths.    
+    PROJECT_PATH+"/../SimplySchedule".replace('\\','/'),
+    PROJECT_PATH+"/../books".replace('\\','/'),
+    PROJECT_PATH+"/../upload".replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
