a3cf1d9deb24b3bc985114b92a45f80a72d5c5c0
Jac <jacalata@gmail.com>
2014-01-19 23:16:10 -0800

    github clusterfuck
    seems to not recognise this repo as being from the original. Let's see
    what happens.

---
+++ site-packages/newrelic/newrelic-admin-script.py
@@ -0,0 +1,10 @@
+#!C:\Dropbox\code\optimization\SimplySchedule\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'newrelic==2.10.0.8','console_scripts','newrelic-admin'
+__requires__ = 'newrelic==2.10.0.8'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('newrelic==2.10.0.8', 'console_scripts', 'newrelic-admin')()
+    )
